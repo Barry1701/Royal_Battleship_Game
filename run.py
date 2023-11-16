8632fc6a8b29e2c650babcfa4367617fe181d822
Barry1701 <>
2023-11-16 14:54:56 +0000

    Initial commit: Add make_board function with specified size.

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def make_board(size):
+    """
+    Create a game board filled with "0" characters
+    using list comprehension.
+    """
+    return[["0" for _ in range(size)] for _ in range(size)]

447744dd8f89d43d78a70fea642e50ca9eb18dd2
Barry1701 <>
2023-11-16 17:04:18 +0000

    Add player_turn function to execute a player's turn

--- run.py
+++ run.py
@@ -36,3 +36,37 @@ def hit_shot(board, guess_row, guess_col):
     Check if a shot hit a ship.
     """
     return board[guess_row][guess_col] == "$"
+
+def player_turn(board, player_score):
+    """
+    This function handles a player's turn in the game.it prompts a player
+    to pick a row and a column, checks the validity of the input, updates
+    the board game accordingly. It also checks if the player hit a ship,
+    increment the players score and prints the updated score.
+    """
+    while True:
+        try:
+            guess_row = int(input("Pick a row (0,4): "))
+            guess_col = int(input("Pick a column (0,4): "))
+            if not (0 <= guess_row <= 4) or not (0 <= guess_col <= 4):
+                raise ValueError("Values must be numbers between 0 & 4!")
+            break
+        except ValueError:
+            print("You must enter a number!")
+
+    if board[guess_row][guess_col] == "*" or board[guess_row][guess_col] == "X":
+        print("You can't guess the same coordinates twice!")
+        return False
+
+    if hit_shot(board, guess_row, guess_col):
+        print("Player got a Hit!")
+        board[guess_row][guess_col] == "*"
+        player_score += 1
+        print_scores(player_score, 0)
+        return True
+    else:
+        print("Player missed this time!")
+        board[guess_row][guess_col] == "X"
+        return False
+
+

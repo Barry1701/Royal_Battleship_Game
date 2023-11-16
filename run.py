712503877791170d5d063527ecb7ee2d3cf90ffa
Barry1701 <>
2023-11-16 15:35:17 +0000

    Add print_board function with show_ships flag

--- run.py
+++ run.py
@@ -6,3 +6,15 @@ def make_board(size):
     using list comprehension.
     """
     return[["0" for _ in range(size)] for _ in range(size)]
+
+def print_board(board, show_ships = False):
+    """
+    Print the game board.
+    """
+    for row in board:
+        for cell in row:
+            if cell == "$" and not show_ships:
+                print("0", end = " ")
+            else:
+                print(cell, end = " ")
+        print()

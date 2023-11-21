28f64407b0e5187443cde3608a346bbdd12f5436
Barry1701 <>
2023-11-21 13:03:53 +0000

    Run code through PEP8 Python Validator.

--- run.py
+++ run.py
@@ -1,23 +1,26 @@
 import random
 
+
 def make_board(size):
     """
     Create a game board filled with "0" characters
     using list comprehension.
     """
-    return[["0" for _ in range(size)] for _ in range(size)]
+    return [["0" for _ in range(size)] for _ in range(size)]
 
-def print_board(board, show_ships = False):
+
+def print_board(board, show_ships=False):
     """
     Print the game board.
     """
     for row in board:
         for cell in row:
             if cell == "$" and not show_ships:
-                print("0", end = " ")
+                print("0", end=" ")
             else:
-                print(cell, end = " ")
+                print(cell, end=" ")
         print()
+
 
 def place_ships(board, num_ships):
     """
@@ -30,12 +33,14 @@ def place_ships(board, num_ships):
             if board[row][col] == "0":
                 board[row][col] = "$"
                 break
-    
+
+
 def hit_shot(board, guess_row, guess_col):
     """
     Check if a shot hit a ship.
     """
     return board[guess_row][guess_col] == "$"
+
 
 def player_turn(board, player_score, computer_score):
     """
@@ -55,13 +60,11 @@ def player_turn(board, player_score, computer_score):
         guess_row = int(user_input_row)
         guess_col = int(user_input_col)
 
-        if not(0 <= guess_row <= 4) or not (0 <= guess_col <= 4):
+        if not (0 <= guess_row <= 4) or not (0 <= guess_col <= 4):
             print("Invalid Input. Choose a number form 0 to 4")
             continue
 
         break
-        
-
     if board[guess_row][guess_col] == "*" or board[guess_row][guess_col] == "X":
         print("You can't guess the same coordinates twice!")
         return False
@@ -70,27 +73,27 @@ def player_turn(board, player_score, computer_score):
         print(f"Player Guessed: ({guess_row}, {guess_col}) - Bullseye! You got a Hit!")
         board[guess_row][guess_col] = "*"
         player_score[0] += 1
-        
         return True
     else:
         print(f"Player Guesses: ({guess_row}, {guess_col}) - Missed!")
         board[guess_row][guess_col] = "X"
         return False
 
+
 def computer_turn(board, computer_score, player_score):
     """
-    This function handles the computer's turn in the game. It generates random
-    coordinates for the computer's guess, checks if it's a hit, updates the game
-    board, and prints the updated scores.
+    This function handles the computer's turn in the game. 
+    It generates random coordinates for the computer's guess, 
+    checks if it's a hit, updates the game board, 
+    and prints the updated scores.
     """
-    guess_row = random.randint(0, len(board) -1)
-    guess_col = random.randint(0, len(board[0]) -1)
+    guess_row = random.randint(0, len(board) - 1)
+    guess_col = random.randint(0, len(board[0]) - 1)
 
     if hit_shot(board, guess_row, guess_col):
         print(f"Computer Guessed: ({guess_row}, {guess_col}) - Computer got a hit!")
         board[guess_row][guess_col] = "*"
         computer_score[0] += 1
-        
         return True
 
     else:
@@ -98,11 +101,13 @@ def computer_turn(board, computer_score, player_score):
         board[guess_row][guess_col] = "X"
         return False
 
+
 def separator_line():
     """
     Function print a separator line
     """
     print("-" * 50)
+
 
 def print_scores(player, computer):
     """
@@ -110,7 +115,8 @@ def print_scores(player, computer):
     """
     print(f"After this round the scores are:\nPlayer: {player[0]}. Computer: {computer[0]}")
 
-def end_game_message(player_name, player_score, computer_score ):
+
+def end_game_message(player_name, player_score, computer_score):
     """
     Display the end game message based on the winner
     """
@@ -122,6 +128,7 @@ def end_game_message(player_name, player_score, computer_score ):
         print("Oh No! Computer takes the win.\nGive it another shot!")
 
     separator_line()
+
 
 def new_game(player_board, computer_board, player_score, computer_score):
     """
@@ -150,8 +157,6 @@ def main():
     player_name = input("Enter your name: ")
 
     while True:
-
-
         player_board = make_board(5)
         computer_board = make_board(5)
 
@@ -164,7 +169,7 @@ def main():
         while True:
             separator_line()
             print(f"{player_name}'s Board:")
-            print_board(player_board, show_ships = True)
+            print_board(player_board, show_ships=True)
 
             separator_line()
             print("Computer's Board:")
@@ -187,12 +192,9 @@ def main():
         choice = input("Press Enter to start a New Game.\nTo exit the game, type 'e' and press Enter:")
         if choice.lower() == "e":
             break
-            
         else:
             new_game(player_board, computer_board, player_score, computer_score)
 
-
-        
 
 if __name__ == "__main__":
     main()
